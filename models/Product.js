87ef4744d9e16617d938c687f1f43c8840511f63
Anthony Nguyen <anthonynguye56@gmail.com>
2023-11-01 22:41:01 -0700

    init commit with pseudocode

---
+++ models/Product.js
@@ -0,0 +1,29 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    // EXACT NAMES!!
+    // id, int not null primary key auto increment
+    // product name string not null
+    // price decimal not null (constraint? validate?)
+    // category_id foreign key
+    // stock int not null default value of 10 (validate numeric)
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
---
+++ routes/api/category-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  // get all from model
+  // then send it back to the user
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  // specify by ID
+  // get one category
+  // some req.param.nonsense
+  // send it back to the user
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  // get the body and get the contents and insert into sequelize
+  // category(DOT) create
+  // return it back as res.json
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  // taking in an id as a parameter and also receiving a req.body
+  // sequelize update
+  // return a res.json
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  // DESTROY
+  // destroying based off the req.params.id
+  // res.json to let the server
+});
+
+module.exports = router;
